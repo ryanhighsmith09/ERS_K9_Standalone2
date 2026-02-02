8867d36443fcd4d2ea3af529061e8a3994bf038b
ryanhighsmith09 <ryanhighsmith09@gmail.com>
2026-02-02 05:50:17 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,21 @@
 
+fx_version 'cerulean'
+game 'gta5'
+
+author 'ERS Development'
+description 'ERS Standalone K9 v4.2 (Vehicle Bark Entry)'
+version '4.2.0'
+
+ui_page 'html/index.html'
+
+client_scripts {'client.lua'}
+server_scripts {'server.lua'}
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/app.js',
+    'sounds/bark.ogg',
+    'sounds/growl.ogg',
+    'sounds/attack.ogg'
+}

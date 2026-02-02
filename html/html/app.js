63831ad2f6b2ef1917c6da11cd59c954223af44c
ryanhighsmith09 <ryanhighsmith09@gmail.com>
2026-02-02 05:54:35 -0600

    Update app.js

--- html/html/app.js
+++ html/html/app.js
@@ -1,1 +1,14 @@
+function send(action){
+ fetch(`https://${GetParentResourceName()}/radial`,{
+  method:'POST',
+  headers:{'Content-Type':'application/json'},
+  body:JSON.stringify({action})
+ })
+}
+function closeMenu(){
+ fetch(`https://${GetParentResourceName()}/close`,{method:'POST'})
+}
+window.addEventListener('message',e=>{
+ document.body.style.display = e.data.open ? 'block' : 'none'
+})
 
